ae372570ef2afeaaeb49c15b2a37249f0924becc
R4f1lds <rafilds66@gmail.com>
2025-01-18 17:18:22 -0300

    adicionando codigo js

---
+++ src/Js/script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelectorAll(".atualizar-conselho");
+const conselho = document.querySelectorAll(".conselho-id");
+const descricao = document.querySelectorAll(".conselho-descricao");
+
+async function pegarDadosApi(){
+    
+}
+
